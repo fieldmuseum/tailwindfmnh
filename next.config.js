b0c2af28abf961cec2ecd7a49a12ece868a81a0e
BlackMG27 <oike279@gmail.com>
2023-07-24 14:20:19 -0500

    created a colors branch and added colors from the Field Style Guide

--- next.config.js
+++ next.config.js
@@ -4,4 +4,24 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, {
+  theme: {
+    colors: {
+      "field-blue": "#0a46e6",
+      "link-blue": "#3d57cc",
+      "success-green": "#53b59e",
+      "warning-red": "#d44235",
+      "field-black": "#0f0f14",
+      "field-gray-darker": "#333336",
+      "field-gray": "#6a6a71",
+      "field-gray-light": "#c9cacc",
+      "field-gray-lighter": "f0f3f3",
+      "field-orange": "#f29f77",
+      "field-purple": "b274a7",
+      "field-blue-light": "#3d57cc",
+      "field-green": "37816e",
+      "field-yellow": "#f5d689",
+      "field-aqua": "#6eb3d5"
+    }
+  }
+}
