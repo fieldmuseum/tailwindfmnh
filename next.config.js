3ef0eb30684ebf5d3f04a977e937485f399f3cb3
BlackMG27 <oike279@gmail.com>
2023-07-24 15:15:35 -0500

    added the style guide fonts and removed the defaults for better performance

--- next.config.js
+++ next.config.js
@@ -4,4 +4,28 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, {
+  //removes unwanted default fonts for better perfomance
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'druk': ["Druk Bold", 'display'],
+      'graphik-med': ['Graphik Medium', 'sans-serif'],
+      'graphik-reg': ['Graphik Regular', 'sans-serif'],
+      'graphik-bold': ["Graphik Semi-Bold", 'sans-serif'],
+      'lyon-bold': ["Lyon-Display Bold", 'serif'],
+      "lyon-reg": ["Lyon-Display Regular", "serif"],
+      "lyon-italic": ["Lyon-Display Italic", 'serif']
+    },
+  },
+  //removes unwanted and unused CSS in production for better performance 
+  purge: {
+    enabled: true,
+    content: [
+      './**/*.html'
+    ]
+  }
+}
