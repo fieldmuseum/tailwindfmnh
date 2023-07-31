658d4f24194adde028df804df6633b2a4f786251
Pete Herbst <pete@peteherbst.com>
2023-07-31 16:02:51 -0500

    Fix issue with tailwind not loading

--- src/components/buttons/ButtonPrimary.tsx
+++ src/components/buttons/ButtonPrimary.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function ButtonPrimary() {
   return (
-    <button className="mt-5">
+    <button className="mt-5 bg-fieldBlue text-fieldGrayLighter">
       <Link href={'#'}>
-      Learn More
+        Learn More
       </Link>
     </button>
   )
