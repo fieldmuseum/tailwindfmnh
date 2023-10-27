0b797a05fbdbd707913af37f1833ba6e3e10b0d1
BlackMG27 <oike279@gmail.com>
2023-10-27 17:25:59 -0500

    edited the shuffle button text color.

--- src/components/buttons/ButtonShuffle.tsx
+++ src/components/buttons/ButtonShuffle.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function ButtonShuffle() {
     return(
-        <button className=" mt-4 font-graphik font-[500] text-lg/[1.8rem] mt-3 px-[14px] py-[9px] align-middle inline-flex items-center relative hover:bg-fieldGrayLighter before:content-shuffleButton before:alt-['Shuffle Icon'] before:top-[27%] before:left-[-16%] before:absolute">
+        <button className=" mt-4 font-graphik font-[500] text-lg/[1.8rem] mt-3 px-[14px] py-[9px] align-middle inline-flex items-center relative hover:bg-fieldGrayLighter before:content-shuffleButton before:alt-['Shuffle Icon'] before:top-[27%] before:left-[-16%] before:absolute text-fieldBlue">
             <Link href={'#'}>
                 {/* <span className="mr-3  top-[27%] left-[-16%] absolute">
                     <Image src='https://www.datocms-assets.com/44232/1662998329-shuffle-icon.svg' alt='Shuffle Icon' width={24} height={18}/>
