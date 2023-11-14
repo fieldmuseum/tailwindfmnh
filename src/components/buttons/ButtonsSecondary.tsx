e03abf4cc09153611e70bb506aa6fe74a862a01c
BlackMG27 <oike279@gmail.com>
2023-11-14 13:09:21 -0600

    super close to solving animation. just need get through :before pseudo class.

--- src/components/buttons/ButtonsSecondary.tsx
+++ src/components/buttons/ButtonsSecondary.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function ButtonSecondary() {
+  const [isHovered, setIsHovered] = useState(false)
   return (
-    <button className='mt-5 btn border border-fieldBlue text-fieldBlue font-graphik font-[500] text-lg/[1.8rem] z-10 before:content-[""] before:block before:w-full before:animate-wipeIn after:hidden hover:before:btn-primary'>
-      
+    <button className={`mt-5 btn  relative border-2 border-fieldBlue text-fieldBlue font-graphik font-[500] text-lg/[1.8rem] z-10 block hover:bg-transparent before:content-[""] before:block before:absolute before:bg-fieldBlue before:border-2 before:border-fieldBlue before:text-fieldGrayLighter before:w-full before:h-full hover:before:btn-primary before:top-0 before:left-0 before:z-[-1] before:px-[26px] before:py-[10px] before:translate-y-[102%] before:transtion-all before:duration-200 ${isHovered && 'before:animate-wipeIn' ? 'before:animate-wipeIn': ''}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
       <Link href={"#"}>
         Secondary Button
       </Link>
