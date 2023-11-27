9790623c3e4c2c2290d8a7a68cce7f70cb0f8fad
BlackMG27 <oike279@gmail.com>
2023-11-27 11:12:44 -0500

    fixed the titled lists fonts to match FM Styleguide

--- src/components/lists/titled-list.tsx
+++ src/components/lists/titled-list.tsx
@@ -5,19 +5,19 @@ export default function TitledList () {
         <dl className={` def-list md:flex md:flex-wrap md:columns-2 font-graphik text-fieldGrayDarker`}>
                     <div>
                         <dt>
-                            <h6>CHICAGO RESIDENTS</h6>
+                            CHICAGO RESIDENTS
                         </dt>   
                         <dd>Chicago residents can save $5 or more on the price of their ticket.</dd>
                     </div>
                     <div>
                         <dt>
-                            <h6>MUSEUMS FOR ALL</h6>
+                            MUSEUMS FOR ALL
                         </dt>
                         <dd>Illinois families with EBT(Link) or WIC Cards receive $3 General Admission for up to six people.</dd>
                     </div>
                     <div>
                         <dt>
-                            <h6>ILLINOIS TEACHERS</h6>
+                            ILLINOIS TEACHERS
                         </dt>
                         <dd>Illinois teachers (pre-K through 12) enjoy complementary General Admission.</dd>
                     </div>
