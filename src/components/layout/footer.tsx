b0bf45e96e68e56651beede121cee6ac5acb2439
BlackMG27 <oike279@gmail.com>
2024-02-02 17:19:38 -0600

    added styles to footer-top

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,7 +6,17 @@ export default function Footer(){
   return(
     <footer className='footer js-footer'>
       <h2 className="st-only">Footer</h2>
-
+      <div className="footer-top">
+        <div className="row">
+          <p>MUSEUM OPEN DAILY, 9AM-5PM, LAST ENTRY 4PM</p>
+          <p>CLOSED THANKSGIVING DAY AND CHRISTMAS DAY</p>
+          <p>MUSEUM IS LOCATED AT 1400 S. DUSABLE LAKE SHORE DRIVE, CHICAGO, IL 60605</p>
+        </div>
+        <div className="row">
+          
+        </div>
+      </div>
+      <div className="footer-bottom"></div>
     </footer>
   )
 }
